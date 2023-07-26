e68492bfc993d64ff55cd165943e1070f1090d29
trouvaille6 <138932865+trouvaille6@users.noreply.github.com>
2023-07-26 22:59:16 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,1 +1,16 @@
+/*
+Create a class called Node to represent the nodes of the linked list. Each node should contain an integer value and a reference to the next node in the list.
+Create a class called SinglyLinkedList to represent the linked list itself. The class should have the following methods:
+a. insertFront(int value): Inserts a new node with the given value at the front of the list.
+b. insertBack(int value): Inserts a new node with the given value at the end of the list.
+c. delete(int value): Deletes the first occurrence of the node with the given value from the list, if it exists.
+d. search(int value): Returns true if the list contains a node with the given value, otherwise returns false.
+e. size(): Returns the number of nodes in the list.
+f. isEmpty(): Returns true if the list is empty, otherwise returns false.
+g. printList(): Prints the elements of the list in order.
+Instructions:
 
+Implement the Node class with the required fields and constructor.
+Implement the SinglyLinkedList class with the methods mentioned above.
+Test your implementation with various test cases to ensure the correctness of your code.
+*/
